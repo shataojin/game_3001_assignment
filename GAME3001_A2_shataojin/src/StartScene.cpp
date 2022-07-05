e6b83de0242cbbbcd16462cfdbd405bb48476f2a
shataojin <stj2062287@gmail.com>
2022-07-04 21:06:49 -0400

    base build and added text in start scene

---
+++ GAME3001_A2_shataojin/src/StartScene.cpp
@@ -0,0 +1,91 @@
+#include "StartScene.h"
+#include <algorithm>
+#include "Game.h"
+#include "glm/gtx/string_cast.hpp"
+#include "EventManager.h"
+
+StartScene::StartScene()
+{
+	StartScene::Start();
+}
+
+StartScene::~StartScene()
+= default;
+
+void StartScene::Draw()
+{
+	DrawDisplayList();
+}
+
+void StartScene::Update()
+{
+	UpdateDisplayList();
+}
+
+void StartScene::Clean()
+{
+	RemoveAllChildren();
+}
+
+void StartScene::HandleEvents()
+{
+	EventManager::Instance().Update();
+
+	// Keyboard Events
+	if(EventManager::Instance().IsKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		Game::Instance().Quit();
+	}
+
+	if(EventManager::Instance().IsKeyDown(SDL_SCANCODE_1))
+	{
+		Game::Instance().ChangeSceneState(SceneState::PLAY);
+	}
+}
+
+void StartScene::Start()
+{
+	const SDL_Color blue = { 0, 0, 255, 255 };
+	m_pStartLabel = new Label("START SCENE", "Consolas", 60, blue, glm::vec2(400.0f, 40.0f));
+	m_pStartLabel->SetParent(this);
+	AddChild(m_pStartLabel);
+
+	m_pInstructionsLabel = new Label("abababababababa¼ÓËµÃ÷", "Consolas", 17, blue, glm::vec2(400.0f, 500.0f));
+	m_pInstructionsLabel->SetParent(this);
+	AddChild(m_pInstructionsLabel);
+
+	//
+	m_TextLabels = new Label("name:taojin sha", "Consolas", 20, blue, glm::vec2(90.0f, 560.0f));
+	m_TextLabels->SetParent(this);
+	AddChild(m_TextLabels);
+
+	m_TextID = new Label("studentID: 101334639", "Consolas", 20, blue, glm::vec2(115.0f, 580.0f));
+	m_TextID->SetParent(this);
+	AddChild(m_TextID);
+	//
+	// Start Button
+
+	// Start Button
+	m_pStartButton = new Button();
+	m_pStartButton->GetTransform()->position = glm::vec2(400.0f, 400.0f); 
+
+	m_pStartButton->AddEventListener(Event::CLICK, [&]()-> void
+	{
+		m_pStartButton->SetActive(false);
+		Game::Instance().ChangeSceneState(SceneState::PLAY);
+	});
+	
+	m_pStartButton->AddEventListener(Event::MOUSE_OVER, [&]()->void
+	{
+		m_pStartButton->SetAlpha(128);
+	});
+
+	m_pStartButton->AddEventListener(Event::MOUSE_OUT, [&]()->void
+	{
+		m_pStartButton->SetAlpha(255);
+	});
+	AddChild(m_pStartButton);
+
+	ImGuiWindowFrame::Instance().SetDefaultGuiFunction();
+}
+
---
+++ GAME3001_A2_shataojin/src/StartScene.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef __START_SCENE__
+#define __START_SCENE__
+
+#include "Scene.h"
+#include "Label.h"
+#include "Ship.h"
+#include "Button.h"
+
+class StartScene final : public Scene
+{
+public:
+	StartScene();
+	~StartScene() override;
+
+	// Inherited via Scene
+	virtual void Draw() override;
+	virtual void Update() override;
+	virtual void Clean() override;
+	virtual void HandleEvents() override;
+	virtual void Start() override;
+	
+private:
+	Label* m_pStartLabel{};
+	Label* m_pInstructionsLabel{};
+	Label* m_TextLabels{};
+	Label* m_TextID{};
+	Ship* m_pShip{};
+
+	Button* m_pStartButton{};
+};
+
+#endif /* defined (__START_SCENE__) */

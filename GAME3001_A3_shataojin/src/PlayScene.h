d4e81d5f2a2b3642801982eb913499380c83b0cb
shataojin <stj2062287@gmail.com>
2022-07-25 23:31:42 -0400

    based build

---
+++ GAME3001_A3_shataojin/src/PlayScene.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef __PLAY_SCENE__
+#define __PLAY_SCENE__
+
+#include "Obstacle.h"
+#include "Scene.h"
+#include "PathNode.h"
+#include "Starship.h"
+#include "Target.h"
+#include "LOSMode.h"
+#include "TiledLevel.h"
+
+class PlayScene : public Scene
+{
+public:
+	PlayScene();
+	~PlayScene() override;
+
+	// Scene LifeCycle Functions
+	virtual void Draw() override;
+	virtual void Update() override;
+	virtual void Clean() override;
+	virtual void HandleEvents() override;
+	virtual void Start() override;
+private:
+	// IMGUI Function
+	void GUI_Function();
+	std::string m_guiTitle;
+	bool m_isGridEnabled;
+
+	// Input Control
+	int m_pCurrentInputType{};
+	void GetPlayerInput();
+	void GetKeyboardInput();
+
+	// Game Objects
+	Target* m_pTarget;
+	Starship* m_pStarship;
+	TiledLevel* m_pLevel;
+	std::vector<Obstacle*> m_pObstacles;
+
+	void BuildObstaclePool();
+
+	// Create our Division Scheme (Grid of PathNodes)
+	std::vector<PathNode*> m_pGrid;
+	void m_buildGrid();
+	void m_toggleGrid(bool state);
+
+	bool m_checkAgentLOS(Agent* agent, DisplayObject* target_object);
+	bool m_checkPathNodeLOS(PathNode* path_node, DisplayObject* target_object);
+	void m_checkAllNodesWithTarget(DisplayObject* target_object);
+	void m_checkAllNodesWithBoth();
+	void m_setPathNodeLOSDistance(int dist);
+	LOSMode m_LOSMode{};
+
+	void m_clearNodes();
+	int m_pathNodeLOSDistance;
+};
+
+#endif /* defined (__PLAY_SCENE__) */
